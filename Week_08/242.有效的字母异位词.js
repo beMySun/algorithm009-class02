773696d3ed80526bea8ac9653f05e068dafa16b8
Yongkang Yan <yongkang.yan@shopee.com>
2020-07-11 21:34:49 +0800

    feat： add week 8

---
+++ Week_08/242.有效的字母异位词.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=javascript
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// 方法1:
+// var isAnagram = function (s, t) {
+//   return s.split('').sort().join('') == t.split('').sort().join('');
+// };
+
+
+// 方法2: Set
+var isAnagram = function(s, t) {
+  if(s.length !== t.length) return false;
+  
+  var ary = new Array(26).fill(0);
+
+  for(var i = 0; i < s.length; i++) {
+      ary[s[i].charCodeAt(0) - 97]++;
+      ary[t[i].charCodeAt(0) - 97]--;
+  }
+  for(var i = 0; i < ary.length; i++) {
+      if(ary[i] !== 0) return false;
+  }
+  return true
+};
+// @lc code=end
+
